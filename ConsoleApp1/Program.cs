b1864296cb2e44a0b48ac2c229c25a96fbed6731
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-26 23:45:57 +0530

    Added:CSV libraries

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+
+namespace ConsoleApp1
+{
+    public class AddressBookRecords 
+    { 
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            var list = new List<AddressBookRecords>()
+            {
+                new AddressBookRecords{ID = 1,Name = "Nutan"},
+                new AddressBookRecords{ID = 2,Name = "Pooja"},
+                new AddressBookRecords{ID = 3,Name = "Richa"}
+            };
+            
+            using(var writer = new StreamWriter {@"" })
+            {
+                using (var helper = new CsvWriter { writer, Cultureinfo.Inv }) {
+                    helper.WriteRecord<AddressBookRecords>();
+                }
+            }
+        }
+    }
+}
